e2676b2e1d92b03c36480bca66060f17118a63b3
Martín A. Frangul <postguerra@gmail.com>
2021-09-20 18:33:02 +0200

    Fent ex 3

--- Nivell 1/javascript.js
+++ Nivell 1/javascript.js
@@ -1,24 +1,41 @@
-let vocales = ['A', 'E', 'I', 'O', 'U']
-let myNom = ['M', 'A', 'R', 3, 'T', 'I', 'N']
-let trobat = true;
+
+//EXERCICI 1
+
+// let myNom = new Array('M', 'A', 'R', 'T', 'I', 'N')
+
+// for (i=0; i < myNom.length; i++) {
+//     console.log(myNom[i]);
+// }
 
 
-for (i = 0; i < myNom.length; i++){
-    trobat = true;
-    for (x = 0; x < vocales.length; x++) {
-        if (myNom[i] == vocales[x] && trobat == true) {
-            console.log('He trobat la vocal:' + vocales[x])
-            trobat = false;  
-        }
-    }
-        if (trobat == true && isNaN(myNom[i])) {
-        console.log('He trobat la consonant:' + myNom[i])
-        trobat = false;  
-        }
 
-        if (trobat == true && !isNaN(myNom[i])) {
-        console.log('Els noms de persones no contenen el numero:' + myNom[i])
-        trobat = false;
-        }
-    }
+// EXERCICI 2
+
+// let vocales = ['A', 'E', 'I', 'O', 'U']
+// let myNom = ['M', 'A', 'R', 3, 'T', 'I', 'N']
+// let trobat = true;
+
+
+// for (i = 0; i < myNom.length; i++){
+//     trobat = true;
+//     for (x = 0; x < vocales.length; x++) {
+//         if (myNom[i] == vocales[x] && trobat == true) {
+//             console.log('He trobat la vocal:' + vocales[x])
+//             trobat = false;  
+//         }
+//     }
+//         if (trobat == true && isNaN(myNom[i])) {
+//         console.log('He trobat la consonant:' + myNom[i])
+//         trobat = false;  
+//         }
+
+//         if (trobat == true && !isNaN(myNom[i])) {
+//         console.log('Els noms de persones no contenen el numero:' + myNom[i])
+//         trobat = false;
+//         }
+//     }
+
+//AIXÓ TAMBÉ ES PODRÍA FER AMB includes()
+
+//EXERCICI 3
 
